a8031eee5aefc7e03bb011ec51f4f3c77672d4dd
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-04-26 15:55:39 -0300

    add functionality on introducao slide

--- js/app.js
+++ js/app.js
@@ -64,3 +64,20 @@ $('.mobile-btn').click(function () {
   $(this).toggleClass('active');
   $('.mobile-menu').toggleClass('active');
 });
+
+$('.slide > :first').addClass('active');
+
+function rotateSlide() {
+  let activeSlide = $('.slide > .active'),
+    nextSlide = activeSlide.next();
+
+  if (nextSlide.length == 0) {
+    nextSlide = $('.slide > :first');
+  }
+  activeSlide.removeClass('active');
+  nextSlide.addClass('active');
+}
+
+setInterval(() => {
+  rotateSlide();
+}, 2000);
